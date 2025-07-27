eb00cd9289b4bc64f87b076db5b2c8d26a0533b7
HASHI-MD-TEST <hasithnirmal501@gmail.com>
2025-07-27 15:58:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "iM9GkaaC#b7_MvhiUSXNyCd5IkGJrPW_xkSkveogOOSy9VNROcv4",
-MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_5278.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true"
